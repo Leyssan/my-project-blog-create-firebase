f46f6726ae770449c194c8d4d787a0b362d4eddd
Leisan <107116722+Leyssan@users.noreply.github.com>
2022-08-05 17:44:01 +0300

    add posts and add button 'Back'

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,15 +1,27 @@
-const Blog = () => {
-    return(
-        <div>
-            <h1>
-                Blog
-            </h1>
-            <p>
-                Hello, Misha
-            </p>
-        </div>
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
-        
+const Blog = () => {
+    const [blog, setBlog] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => setBlog(data))
+    }, []);
+
+    return (
+        <div>
+            <h1>Our news</h1>
+            {
+                blog.map(x => (
+                    <Link key={x.id} to={`/blog/${x.id}`}>
+                        <li>{x.title}</li>
+                    </Link>
+                ))
+            }
+        </div>
     )
 }
+
 export {Blog}
--- src/pages/DynamicPage.js
+++ src/pages/DynamicPage.js
@@ -1,15 +1,29 @@
-const DynamicPage = () => {
-    return(
-        <div>
-            <h1>
-               Dinamic
-            </h1>
-            <p>
-                Hello, Misha
-            </p>
-        </div>
+import { useParams, Link, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
-        
+const DynamicPage = () => {
+    const {id} = useParams();
+    const [blog, setBlog] = useState(null);
+    const navigate = useNavigate()
+const backButton = () => navigate(-1)
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setBlog(data))
+    }, [id]);
+
+    return (
+        <div>
+            {blog && (
+                <>
+                    <h1>{blog.title}</h1>
+                    <p>{blog.body}</p>
+                    <Link to={`/blog/${id}/edit`}>Edit this post</Link>
+                </>
+            )}
+            <button onClick={backButton}>Back</button>
+        </div>
     )
 }
 export {DynamicPage}
